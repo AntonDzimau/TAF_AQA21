ee602b41f798f7eb84247294a97128d342c714a2
AntonDzimau <chehov.anton96@gmail.com>
2022-10-03 23:49:10 +0300

    Corrected remarks from 10/03/2022.

--- src/test/java/DivHW.java
+++ src/test/java/DivHW.java
@@ -3,59 +3,118 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DivHW extends BaseTest {
-    @Test(groups = "smoke", priority = 1)
-    public void testDivInt() {
-        Assert.assertEquals(calculator.div(4, 2), 2, "Неверный ожидаемый результат...");
+    @Test(groups = "smoke", priority = 1, testName = "Dividing of two positive real numbers.")
+    public void divisionPosIntTest() {
+        Assert.assertEquals(calculator.div(9, 3), 3, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "regression", enabled = false, priority = 10)
-    public void testDivInt1() {
-        Assert.assertEquals(calculator.div(4, 3), 2, "Неверный ожидаемый результат!");
+    @Test(groups = "smoke", priority = 1, testName = "Dividing of two negative real numbers.")
+    public void divisionNegIntTest1() {
+        Assert.assertEquals(calculator.div(-9, -3), 3, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", description = "Тест проверяющий деление 0 на целое число числа на 0.", priority = 2)
-    public void testDivInt2() {
-        Assert.assertEquals(calculator.div(0, 87), 1, "Если ноль разделить на число получится ноль!");
+    @Test(groups = "smoke", priority = 1, testName = "Dividing a positive real number by a negative real number.")
+    public void divisionNegIntTest2() {
+        Assert.assertEquals(calculator.div(9, -3), -3, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", testName = "Division of two real numbers.", priority = 3)
-    public void testDivDouble() {
-        Assert.assertEquals(calculator.div(1.3, 2.0), 0.65, "Неверный ожидаемый результат!");
+    @Test(groups = "smoke", priority = 1, testName = "Dividing a negative real number by a positive real number.")
+    public void divisionNegIntTest3() {
+        Assert.assertEquals(calculator.div(-9, 3), -3, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "regression", timeOut = 750, testName = "Division after some time test.", priority = 1)
-    public void testDivDouble1() throws InterruptedException {
-        Thread.sleep(735);
-        Assert.assertEquals(calculator.div(88888888.4444, 22222222.1111), 4, "Неверный ожидаемый результат.");
+    @Test(groups = "smoke", priority = 2, testName = "Dividing a real numbers with data from provider.", dataProvider = "Data for dividing the real numbers", dataProviderClass = StaticProvider.class, threadPoolSize = 4)
+    public void divisionIntFromDataProvider(int a, int b, int expectedResult) {
+        Assert.assertEquals(calculator.div(a, b), expectedResult, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "regression", invocationCount = 4, invocationTimeOut = 1000, threadPoolSize = 2, testName = "Quadruple division on two streams.", priority = 2)
-    public void testDivWithInvocation() throws InterruptedException {
-        Thread.sleep(900);
-        Assert.assertEquals(calculator.div(999999999, 333333333), 3, "Неверный ожидаемый результат.");
+    @Test(groups = "smoke", priority = 3, testName = "Dividing zero by a positive real number.")
+    public void divisionZeroByPosRealNumberTest() {
+        Assert.assertEquals(calculator.div(0, 8), 0, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 3, testName = "Dividing zero by a negative real number.")
+    public void divisionZeroByNegRealNumberTest() {
+        Assert.assertEquals(calculator.div(0, -8), 0, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 3, testName = "Dividing a positive real number by zero.", expectedExceptions = ArithmeticException.class)
+    public void divisionPosRealNumberByZeroTest() throws ArithmeticException {
+        calculator.div(8, 0);
+    }
+
+    @Test(groups = "smoke", priority = 3, testName = "Dividing a negative real number by zero.", expectedExceptions = ArithmeticException.class)
+    public void divisionNegRealNumberByZeroTest() throws ArithmeticException {
+        calculator.div(-8, 0);
+    }
+
+    @Test(groups = "smoke", priority = 3, testName = "Dividing of two real zeroes.", expectedExceptions = ArithmeticException.class)
+    public void divisionRealZeroesTest() throws ArithmeticException {
+        calculator.div(0, 0);
+    }
+
+    @Test(groups = "smoke", priority = 4, testName = "Dividing of two positive double numbers.")
+    public void divisionPosDoubleTest() {
         Assert.assertEquals(calculator.div(15.6, 3.0), 5.2, "Неверный ожидаемый результат.");
-        Assert.assertEquals(calculator.div(10.0, 0.1), 100, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", dataProvider = "Data for division the real numbers", dataProviderClass = StaticProvider.class, threadPoolSize = 4, priority = 4)
-    public void testDivDataProvider(int a, int b, int expectedResult) {
+    @Test(groups = "smoke", priority = 4, testName = "Dividing of two negative double numbers.")
+    public void divisionNegDoubleTest1() {
+        Assert.assertEquals(calculator.div(-15.6, -3.0), 5.2, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 4, testName = "Dividing a positive double number by a negative double number.")
+    public void divisionNegDoubleTest2() {
+        Assert.assertEquals(calculator.div(15.6, -3.0), -5.2, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 4, testName = "Dividing a negative double number by a positive double number.")
+    public void divisionNegDoubleTest3() {
+        Assert.assertEquals(calculator.div(-15.6, 3.0), -5.2, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 5, testName = "Dividing a double numbers with data from provider.", dataProvider = "Data for dividing the double numbers", dataProviderClass = StaticProvider.class, threadPoolSize = 2, timeOut = 10)
+    public void divisionDoubleFromDataProvider(double a, double b, double expectedResult) {
         Assert.assertEquals(calculator.div(a, b), expectedResult, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", dependsOnMethods = "testDivDataProvider", dataProvider = "Data for division the double numbers", dataProviderClass = StaticProvider.class, threadPoolSize = 2, timeOut = 10, priority = 5)
-    public void testDataProvider(double a, double b, double expectedResult) {
-        Assert.assertEquals(calculator.div(a, b), expectedResult, "Неверный ожидаемый результат.");
+    @Test(groups = "smoke", priority = 6, testName = "Dividing zero by a positive double number.")
+    public void divisionZeroByPosDoubleNumberTest() {
+        Assert.assertEquals(calculator.div(0, 1.2), 0, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", testName = "Div int nums by 0", expectedExceptions = ArithmeticException.class, priority = 6)
-    public void testExceptionsWithIntNumbs() throws ArithmeticException {
-        calculator.div(17, 0);
+    @Test(groups = "smoke", priority = 6, testName = "Dividing zero by a negative double number.")
+    public void divisionZeroByNegDoubleNumberTest() {
+        Assert.assertEquals(calculator.div(0, -1.2), 0, "Неверный ожидаемый результат.");
     }
 
-    @Test(groups = "smoke", dependsOnMethods = "testExceptionsWithIntNumbs", testName = "Div double nums by 0", expectedExceptions = ArithmeticException.class, priority = 7)
-    public void testExceptionsWithDoubleNumbs() {
-        calculator.div(23.7, 0);
-        //Assert.assertEquals(calculator.div(23.7, 0), "Inf");
-        /**Тест ПАДАЕТ, не ловит ошибку ArithmeticException by zero, а пишет актуальным результат - Infinity*/
+    @Test(groups = "smoke", priority = 6, testName = "Dividing a positive real number by zero.")
+    public void divisionPosDoubleNumberByZeroTest() throws ArithmeticException {
+        Assert.assertEquals(calculator.div(1.2, 0), Double.POSITIVE_INFINITY, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 6, testName = "Dividing a negative real number by zero.")
+    public void divisionNegDoubleNumberByZeroTest() throws ArithmeticException {
+        Assert.assertEquals(calculator.div(-1.2, 0), Double.NEGATIVE_INFINITY, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "smoke", priority = 6, testName = "Dividing of two real zeroes.")
+    public void divisionDoubleZeroesTest() throws ArithmeticException {
+        Assert.assertEquals(calculator.div(0.0, 0.0), Double.NaN, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "regression", priority = 10, invocationCount = 4, invocationTimeOut = 1000, threadPoolSize = 2, testName = "Quadruple dividing on two streams.")
+    public void testDivWithInvocation() throws InterruptedException {
+        Thread.sleep(800);
+        Assert.assertEquals(calculator.div(-15.6, 3.0), -5.2, "Неверный ожидаемый результат.");
+        Assert.assertEquals(calculator.div(-15, 5), -3, "Неверный ожидаемый результат.");
+        Assert.assertEquals(calculator.div(-150, -15), 10, "Неверный ожидаемый результат.");
+        Assert.assertEquals(calculator.div(63.6, 2.0), 31.8, "Неверный ожидаемый результат.");
+    }
+
+    @Test(groups = "regression", priority = 10, timeOut = 750, testName = "Dividing of two double after some time test.")
+    public void testDivDouble1() throws InterruptedException {
+        Thread.sleep(732);
+        Assert.assertEquals(calculator.div(15.6, 3.0), 5.2, "Неверный ожидаемый результат.");
     }
 }
--- src/test/java/data/StaticProvider.java
+++ src/test/java/data/StaticProvider.java
@@ -11,22 +11,30 @@ public class StaticProvider {
                 {0, 0, 0},
                 {2, 3, 5}
         };*/
-    @DataProvider(name = "Data for division the real numbers")
+    @DataProvider(name = "Data for dividing the real numbers")
     public static Object[][] dataForDivRealNumbsTest() {
         return new Object[][]{
-                {18, -2, -9},
-                {0, 1, 0},
-                {17, 17, 1},
-                {-23, -1, 23}
+                {7, 7, 1},
+                {-7, 7, -1},
+                {-7, -7, 1},
+                {7, -7, -1},
+                {7, 1, 7},
+                {-7, -1, 7},
+                {-7, 1, -7},
+                {7, -1, -7},
         };
     }
-    @DataProvider(name = "Data for division the double numbers")
+    @DataProvider(name = "Data for dividing the double numbers")
     public static Object[][] dataForDivDoubleNumbsTest() {
         return new Object[][]{
-                {18.9, -3, -6.3},
-                {0.0, 0.2, 0},
-                {21.5, -0.1, -215},
-                {-9.3, -3, 3.1}
+                {1.2, 1.2, 1},
+                {-1.2, 1.2, -1},
+                {-1.2, -1.2, 1},
+                {1.2, -1.2, -1},
+                {1.2, 1.0, 1.2},
+                {-1.2, -1.0, 1.2},
+                {-1.2, 1.0, -1.2},
+                {1.2, -1.0, -1.2},
         };
     }
 }

3ef5a9ed2d8e3776e1ea4adda00975680b284e8e
AntonDzimau <chehov.anton96@gmail.com>
2022-10-06 19:54:08 +0300

    Corrected the additional task of Lesson 6.

--- src/test/java/tests/SmokeTest_LaminateCalc.java
+++ src/test/java/tests/SmokeTest_LaminateCalc.java
@@ -1,10 +1,12 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,40 +21,31 @@ public class SmokeTest_LaminateCalc {
         driver = browsersService.getDriver();
     }
 
-    @Test(priority = 1)
-    public void laminateCalcTest() {
+    @Test(priority = 1, invocationCount = 5)
+    public void laminateCalcTest() throws InterruptedException {
         driver.get("https://calc.by/building-calculators/laminate.html");
-        Select selectLayingMethod = new Select(driver.findElement(By.id("laying_method_laminate")));
-        selectLayingMethod.selectByVisibleText("с использованием отрезанного элемента");
-        WebElement lengthRoom = driver.findElement(By.id("ln_room_id"));
-        lengthRoom.clear();
-        lengthRoom.sendKeys("500");
-        WebElement widthRoom = driver.findElement(By.id("wd_room_id"));
-        widthRoom.clear();
-        widthRoom.sendKeys("400");
-        WebElement lengthPanel = driver.findElement(By.id("ln_lam_id"));
-        lengthPanel.clear();
-        lengthPanel.sendKeys("2000");
-        WebElement widthPanel = driver.findElement(By.id("wd_lam_id"));
-        widthPanel.clear();
-        widthPanel.sendKeys("200");
-        WebElement directionLaminate = driver.findElement(By.id("direction-laminate-id1"));
-        new Actions(driver).click(directionLaminate).perform();
-        WebElement buttonCalc = driver.findElement(By.className("calc-btn"));
-        buttonCalc.click();
-        WebElement closePopUp = driver.findElement(By.className("mfp-close"));
-        if (closePopUp.isDisplayed()) {
-            closePopUp.click();
+        driver.findElement(By.id("ln_room_id")).clear();
+        driver.findElement(By.id("ln_room_id")).sendKeys("500");
+        driver.findElement(By.id("wd_room_id")).clear();
+        driver.findElement(By.id("wd_room_id")).sendKeys("400");
+        driver.findElement(By.id("ln_lam_id")).clear();
+        driver.findElement(By.id("ln_lam_id")).sendKeys("2000");
+        driver.findElement(By.id("wd_lam_id")).clear();
+        driver.findElement(By.id("wd_lam_id")).sendKeys("200");
+        ((JavascriptExecutor)driver).executeScript("scroll(0,900)");
+        new Select(driver.findElement(By.id("laying_method_laminate"))).selectByVisibleText("с использованием отрезанного элемента");
+        driver.findElement(By.id("direction-laminate-id1")).click();
+        driver.findElement(By.className("calc-btn")).click();
+        if (driver.findElement(By.className("mfp-close")).isDisplayed()) {
+            driver.findElement(By.className("mfp-close")).click();
         }
-       /* WebElement calcResult = driver.findElement(By.className("calc-result"));
-        WebElement pint = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[3]/div[2]/div[1]/span"));
-        System.out.println(pint.getText());
-        System.out.println(pint.getAttribute("value"));*/
+        Thread.sleep(670);
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.calc-result span")).getText(),"53");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.calc-result > div ~ div > span")).getText(),"7");
     }
 
     @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(1000);
+    public void tearDown() {
         driver.quit();
     }
 }

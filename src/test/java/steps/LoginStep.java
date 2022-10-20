c33e0450cfdfb575be27ccd5200c26af4273a4d8
AntonDzimau <chehov.anton96@gmail.com>
2022-10-20 16:25:10 +0300

    Lesson 10 allure report

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -1,6 +1,7 @@
 package steps;
 
 import baseEntities.BaseStep;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import pages.DashboardPage;
 import pages.LoginPage;
@@ -13,7 +14,7 @@ public class LoginStep extends BaseStep {
         loginPage = new LoginPage(driver);
     }
 //просто метод для логина, мы хз какие данные и что вводится, корректно или нет
-
+    @Step
     public void login(String email, String psw) {
         loginPage.setEmail(email);
         loginPage.setPsw(psw);

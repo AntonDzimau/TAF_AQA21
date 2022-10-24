ea0b0baa531d03c365e9c5810d7798fdddbc59e9
AntonDzimau <chehov.anton96@gmail.com>
2022-10-24 19:43:34 +0300

    allure report

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -14,19 +14,20 @@ public class LoginStep extends BaseStep {
         loginPage = new LoginPage(driver);
     }
 //просто метод для логина, мы хз какие данные и что вводится, корректно или нет
-    @Step
     public void login(String email, String psw) {
         loginPage.setEmail(email);
         loginPage.setPsw(psw);
         loginPage.clickLoginButton();
     }
 
+    @Step
     public DashboardPage loginSuccessful(String email, String psw) {
         login(email, psw);
 
         return new DashboardPage(driver);
     }
 
+    @Step
     public LoginPage loginIncorrect(String email, String psw) {
         login(email, psw);
 

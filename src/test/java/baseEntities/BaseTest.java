1d50218fa7c8e3f0638f6dcb3a1c06e1e513af30
AntonDzimau <chehov.anton96@gmail.com>
2022-10-28 13:58:35 +0300

    added method for take screenshots

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,23 +1,27 @@
 package baseEntities;
 
 import configuration.ReadProperties;
+import io.qameta.allure.Description;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
+import utils.InvokedListener;
 
 
-//@Listeners(Listener.class)
+@Listeners(InvokedListener.class)
 public class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
+
     @BeforeMethod
+    @Description("Настройка")
     public void setUp(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
-        this.setDriverToContext(iTestContext, driver);
-
+        iTestContext.setAttribute("driver", driver);
         driver.get(ReadProperties.getUrl());
 
         loginStep = new LoginStep(driver);
@@ -25,12 +29,8 @@ public class BaseTest {
 
     @AfterMethod
     public void TearDown() {
-
         driver.quit();
     }
 
-    public static void setDriverToContext(ITestContext iTestContext, WebDriver driver) {
-        iTestContext.setAttribute("WebDriver", driver);
-    }
 
 }
